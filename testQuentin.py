8e3e11291ffd91dd4c36f5110ab3e1656dff0a7a
quentin Lang <quentinlang38@gmail.com>
2025-08-13 18:26:19 +0200

    debut gravite

--- testQuentin.py
+++ testQuentin.py
@@ -1,17 +1,25 @@
 import pygame
 import utilitaire
 
-# pygame setup
+# pygame setupdddd
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 speed = 200
+gravityForce = 100
+velocityY=0
 dt = 0
 
-playerPos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+playerPos = pygame.Vector2(300, 360)
 
 while running:
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+    
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
@@ -20,23 +28,22 @@ while running:
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
-    pygame.draw.rect(screen, "blue", (800, playerPos.y, 20, 50), 30)
-    
+    pygame.draw.rect(screen, "blue", (800, 340, 20, 50), 30)
+
+    pygame.draw.circle(screen, "red", (playerPos.x - 250, playerPos.y), 40)    
 
     player = pygame.Rect(playerPos.x, playerPos.y, 40, 100)
     pygame.draw.rect(screen, "red", player, 100)
 
+
     keys = pygame.key.get_pressed()
     if keys:
-        utilitaire.deplacementPlayer(keys, speed, dt, playerPos, screen)
+        utilitaire.deplacementPlayer(keys, speed, dt, playerPos, screen, 40, gravityForce, 360, velocityY)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
+
 
 pygame.quit()
 
--- utilitaire.py
+++ utilitaire.py
@@ -1,19 +1,30 @@
 import pygame
 
 
-def jumpPlayer(playerPos, speed):
-    pass
-    return 0
+def deplacementPlayer(keys, speed, dt, playerPos, screen, playerSize, gravityForce, groundY, velocityY, jumpForce=500):
+    # Limites écran
+    if playerPos.x >= (screen.get_size()[0] - playerSize):
+        playerPos.x = screen.get_size()[0] - playerSize
+    if playerPos.x <= 0:
+        playerPos.x = 0
 
-def deplacementPlayer(keys, speed, dt, playerPos, screen):
-    print(playerPos.x)
-    if playerPos.x >= screen.get_size()[0] or playerPos.x <= 0:
-        speed=0
-    
-    
-
+    # Déplacement horizontal
     if keys[pygame.K_q]:
         playerPos.x -= speed * dt
     if keys[pygame.K_d]:
         playerPos.x += speed * dt
 
+    # Saut (seulement si sur le sol)
+    if keys[pygame.K_SPACE] and playerPos.y >= groundY:
+        velocityY = -jumpForce
+
+    # Gravité
+    velocityY += gravityForce * dt
+    playerPos.y += velocityY * dt
+
+    # Collision sol
+    if playerPos.y >= groundY:
+        playerPos.y = groundY
+        velocityY = 0
+
+    return velocityY
