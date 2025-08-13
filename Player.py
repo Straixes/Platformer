27a62189bd81f66dec4e9afcf7bff4dc60528746
quentin Lang <quentinlang38@gmail.com>
2025-08-13 18:53:49 +0200

    ddd

--- Player.py
+++ Player.py
@@ -9,11 +9,10 @@ class Player:
         self.texture = texture
         self.width = width
         self.height = height
-
+        self.image = pygame.image.load(os.path.join('img', self.texture))
     
     def drawPlayer(self, screen, position):
-        image = pygame.image.load(os.path.join('data', 'img/f{self.texture}'))
-        screen.blit(image, position)
+        screen.blit(self.image, position)
 
     def getSizeSprite(self):
         return (self.width, self.height)
--- testQuentin.py
+++ testQuentin.py
@@ -1,23 +1,26 @@
 import pygame
 import utilitaire
 import Player
-import os
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
+# --- Paramètres ---
 speed = 200
-gravityForce = 2450          # px/s² (valeur typique)
-jumpForce = 950              # px/s (valeur typique)
+gravityForce = 8500          # px/s²
+jumpForce = 1800              # px/s
 playerWidth, playerHeight = 40, 100
 playerPos = pygame.Vector2(300, 260)  # coin haut-gauche
 groundFeetY = 360            # altitude des pieds quand on est posé
 velocityY = 0
 
+# --- Création du joueur (une seule fois) ---
+player = Player.Player("Quentin", 100, "sprite.png", playerWidth, playerHeight)
+
 while running:
-    dt = clock.tick(60) / 1000
+    dt = clock.tick(60) / 1000  # delta time en secondes
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -35,11 +38,13 @@ while running:
 
     # --- Rendu ---
     screen.fill("black")
+    
     # sol visuel
     pygame.draw.line(screen, "white", (0, groundFeetY), (1280, groundFeetY), 2)
-    # joueur
-    player = Player.Player("Quentin", 100, "img/sprite.png", 209, 241)
-    player.drawPlayer(screen, 360)
+    
+    # joueur (position convertie en int)
+    player.drawPlayer(screen, (int(playerPos.x), int(playerPos.y)))
+    
     pygame.display.flip()
 
 pygame.quit()
