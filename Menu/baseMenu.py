31dfeadb92f8f3da87932b336f35b2d4c62888d1
Matroxind <matroxind@gmail.com>
2025-08-15 12:55:16 +0200

    a

--- Menu/baseMenu.py
+++ Menu/baseMenu.py
@@ -1,16 +1,20 @@
+import pygame
 class baseMenu:
     def __init__(self, screen, menuSelect):
         self.screen = screen
         self.menuSelect = menuSelect
         self.buttons = []
+        self.commands = [] #forme (touche,fonction associé)
 
     def updateButtons(self,resolution, mousePos, mouseClick,changeResolution):
         for btn in self.buttons:
             btn.update(resolution ,mousePos, mouseClick, self.screen,changeResolution)
 
+    def checkCommands(self):
     def drawBackground(self):
         self.screen.fill((30, 30, 30))
 
     def updateMenu(self,resolution, mousePos, mouseClick,changeResolution):
         self.drawBackground()
         self.updateButtons(resolution, mousePos, mouseClick,changeResolution)
+        self.checkCommands()
